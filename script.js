595184107311ba8117bde68c85b01b546d85ac70
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-08-22 14:42:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const makeChange = (c) => {
   // your name here
+	let ans={
+		q:0,
+		d:0,
+		n:0,
+		p:0
+	}
+	ans.q=Math.floor(c/25);
+	c=c%25;
+	ans.d=Math.floor(c/10);
+	c%=10;
+	ans.n=Math.floor(c/5);
+	c%=5;
+	ans.p=c;
+	return ans;
+	 
+	
 };
 
 // Do not the change the code below
